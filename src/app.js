a980f9a8aeb6f2e4640ccc804054737eb8c66b8e
Elias <lynohd@hotmail.com>
2026-04-22 23:17:48 +0200

    .

--- src/app.js
+++ src/app.js
@@ -1,42 +1,67 @@
 const container = document.querySelector(".container");
 const list = [];
-const amount = 15;
+let amount = 15;
 
 const baseHue = 0;
 const baseSaturation = 100;
 const baseLightness = 50;
 
 document.addEventListener("DOMContentLoaded", () => {
+
+
+    // Create X amount of circles (div element)
     for (let i = 0; i < amount; i++) {
         const element = document.createElement("div");
+        // give element an id
         element.id = "circle-" + i;
-        element.classList.add("circle");
-        element.addEventListener("click", (e) => {
-            e.remove();
-        });
 
+        //give element a common class
+        element.classList.add("circle");
+
+
+        //add element to a list to keep track of it
+        //TODO: maybe just store id instead of entire DOM node?
         list.push(element);
 
         container.appendChild(element);
     }
+
+
+    document.addEventListener("click", (e) => {
+        if(!e.classList.contains("circle")){
+            return;
+        }
+
+        const element = e.target;
+
+        //remove element from DOM
+        element.remove();
+
+        const index = list.indexOf(element);
+        list.splice(index, 1);
+        amount--;
+    });
+
     loop();
 });
 
 
+
+
+// amount of iterations performed in total
 let iterations = 0;
+
 function loop(){
     for (let i = 0; i < amount; i++) {
         list[i].style.backgroundColor = hsl(iterations, i);
     }
     
     iterations++;
-    
-    if(iterations >= 256){
-        iterations = 0;
-    }
     requestAnimationFrame(loop);
 }
 
+
+//calculate HSL to perform hue shift
 function hsl(iterations, index){
     return `
     hsl(
