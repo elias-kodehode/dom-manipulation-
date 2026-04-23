74e2799ad5d44ff8ecf4741c22545dd3eead0ca7
elias-kodehode <elias96.kodehode@gmail.com>
2026-04-23 11:02:38 +0200

    .

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-const container = document.querySelector(".container");
+const container = document.querySelector(".hue-shift-container");
 const list = [];
 let amount = 15;
 
@@ -7,56 +7,88 @@ const baseSaturation = 100;
 const baseLightness = 50;
 
 document.addEventListener("DOMContentLoaded", () => {
-
-
-    // Create X amount of circles (div element)
-    for (let i = 0; i < amount; i++) {
-        const element = document.createElement("div");
-        // give element an id
-        element.id = "circle-" + i;
-
-        //give element a common class
-        element.classList.add("circle");
-
-
-        //add element to a list to keep track of it
-        //TODO: maybe just store id instead of entire DOM node?
-        list.push(element);
-
-        container.appendChild(element);
-    }
+    // set everything up
+    setup();
 
 
     document.addEventListener("click", (e) => {
-        if(!e.classList.contains("circle")){
-            return;
+        const target = e.target;
+        if(target.classList.contains("circle") && target.id !== "draggable"){
+            onClickCircle(e.target);
+        }
+        
+        if(target.id === "speed-up"){
+            speedModifier++;
+            updateSpeedDisplay();
         }
 
-        const element = e.target;
+        if(target.id === "speed-down"){
+            if(speedModifier > 0)
+                speedModifier--;
+            updateSpeedDisplay();
+        }
 
-        //remove element from DOM
-        element.remove();
+        if(target.id === "reset"){
+            speedModifier = 1;
+            updateSpeedDisplay();
+        }
 
-        const index = list.indexOf(element);
-        list.splice(index, 1);
-        amount--;
+        if(target.id === "add-circle"){
+            createCircle(list.length);
+            amount++;
+        }
     });
 
     loop();
 });
 
+function createCircle(id){
+    const element = document.createElement("div");
+    // give element an id
+    element.id = "circle-" + id;
 
+    //give element a common class
+    element.classList.add("circle");
+    element.setAttribute("draggable", true);
+    //add element to a list to keep track of it
+    //TODO: maybe just store id instead of entire DOM node?
+    list.push(element);
 
+    container.appendChild(element);
+}
+
+function updateSpeedDisplay(){
+    const display = document.getElementById("speed-display");
+    display.innerText = "Current Speed: " + speedModifier;
+}
+
+function onClickCircle(element){
+    //remove element from DOM
+    element.remove();
+
+    const index = list.indexOf(element);
+    list.splice(index, 1);
+    amount--;
+}
+
+function setup(){
+    // Create X amount of circles (div element)
+    for (let i = 0; i < amount; i++) {
+        createCircle(i);
+    }
+    updateSpeedDisplay();
+}
 
 // amount of iterations performed in total
 let iterations = 0;
+let speedModifier = 1;
 
 function loop(){
     for (let i = 0; i < amount; i++) {
-        list[i].style.backgroundColor = hsl(iterations, i);
+        list[i].style.backgroundColor = hsl(iterations % 360, i);
     }
     
-    iterations++;
+    iterations += 1 * speedModifier;
     requestAnimationFrame(loop);
 }
 
@@ -76,3 +108,44 @@ function calculateHue(iterations, index){
     const offset = 10;
     return baseHue + (iterations + (index * offset));
 }
+
+
+
+const draggableCircle = {
+    element: document.getElementById("draggable"),
+    // get the width and height of the draggable element
+    getSize(){
+        const computedStyle = window.getComputedStyle(this.element);
+        return {
+            width: parseInt(computedStyle.width),
+            height: parseInt(computedStyle.height)
+        };
+    },
+    // set the position of element
+    setPosition(x, y){
+        this.element.style.top = y + "px";
+        this.element.style.left = x + "px";
+    },
+    // get position of draggable element
+    getPosition(){
+        return {
+            x: this.element.style.left,
+            y: this.element.style.top
+        }
+    }
+};
+
+
+document.addEventListener("dragend", (e) => {
+    const destination = { 
+        x: e.clientX,
+        y: e.clientY
+    };
+    if(e.target.id === "draggable"){
+        draggableCircle.setPosition(
+            destination.x - draggableCircle.getSize().width / 2,
+            destination.y - draggableCircle.getSize().height / 2
+        );
+    }
+});
+
