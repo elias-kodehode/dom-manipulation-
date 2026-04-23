1341db54751b0324ff7c6387d44937f01ee5def1
elias-kodehode <elias96.kodehode@gmail.com>
2026-04-23 11:32:45 +0200

    .

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 const container = document.querySelector(".hue-shift-container");
+const inputField = document.getElementById("text-input");
+const inputList = document.getElementById("input-list");
 const list = [];
+
+// store list-item elements
+const listItems = [];
+
 let amount = 15;
 
 const baseHue = 0;
@@ -36,6 +42,34 @@ document.addEventListener("DOMContentLoaded", () => {
         if(target.id === "add-circle"){
             createCircle(list.length);
             amount++;
+        }
+
+
+        // add text input into unordered list
+        if(target.id === "input-submit"){
+            if(inputField.value === ""){
+                return;
+            }
+
+            //get li element template
+            const template = document.getElementById("list-item-template");
+
+            const clone = document.importNode(template.content, true);
+            const liElement = clone.querySelector("li");
+            const button = liElement.querySelector("button");
+
+            //change the text of the li element without removing its button child element
+            liElement.childNodes[0].nodeValue = inputField.value;
+
+            //push the instance into a list for state management
+            listItems.push(clone);
+            inputList.appendChild(clone);
+            //reset input element's value
+            inputField.value = "";
+        }
+        // delete input list item
+        if(target.id === "delete-item"){
+            target.parentElement.remove();
         }
     });
 
@@ -141,11 +175,13 @@ document.addEventListener("dragend", (e) => {
         x: e.clientX,
         y: e.clientY
     };
+
     if(e.target.id === "draggable"){
         draggableCircle.setPosition(
             destination.x - draggableCircle.getSize().width / 2,
             destination.y - draggableCircle.getSize().height / 2
         );
     }
+
 });
 
